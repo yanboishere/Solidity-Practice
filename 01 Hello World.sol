f42725c317c421c84b703debbb759e4aeae9047c
Yanbo Wang <yanbotravelaroundworld@gmail.com>
2024-04-28 19:22:23 +0800

    bool

--- 01 Hello World.sol
+++ 01 Hello World.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.6;
 contract HelloWeb3 {
     string public _string="Hello Web3!";
 }
---
+++ 02 Number.sol
@@ -0,0 +1,18 @@
+
+pragma solidity ^0.8.16;
+contract ValueTypest{
+    
+    //布尔值
+    bool public _bool1 = !_bool; //取非
+    bool public _bool2 = _bool && _bool1;//与
+    bool public _bool3 = _bool || _bool1;//或
+    bool public _bool4 = _bool == _bool1;//相等
+    bool public _bool5 = _bool != _bool1;//不相等
+
+    
+
+
+
+
+
+}
