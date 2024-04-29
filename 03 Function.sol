3983ed8a5a6dbfe9227943953dc7617566f43cac
Yanbo Wang <yanbotravelaroundworld@gmail.com>
2024-04-29 16:47:21 +0800

    feat

--- 03 Function.sol
+++ 03 Function.sol
@@ -3,6 +3,20 @@
     contract FunctionTypes{
         uint256 public number = 5;
 
+    // 默认
+    function add() external{
+        number = number + 1;
+    }
+
+    // pure: 纯纯牛马
+    function addPure(uint256 _number) external pure returns(uint256 new_number){
+        new_number = _number + 1;
+    }
+
+    // view: 看客
+    function addView() external view returns(uint256 new_number) {
+        new_number = number + 1;
+    }
 
 
 
