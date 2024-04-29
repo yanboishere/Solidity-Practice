0e796ec432539b263ad40bf22ef709790b917e09
Yanbo Wang <yanbotravelaroundworld@gmail.com>
2024-04-29 19:11:04 +0800

    feat

--- 03 Function.sol
+++ 03 Function.sol
@@ -18,8 +18,24 @@
         new_number = number + 1;
     }
 
+    // internal: 内部
+    function minus() internal {
+        number = number - 1;
+    }
+
+    // 合约内的函数可以调用内部函数
+    function minusCall() external {
+        minus();
+    }
+
+    // payable: 递钱，能给合约支付eth的函数
+    function minusPayable() external payable returns(uint256 balance) {
+        minus();    
+        balance = address(this).balance;
+    }
 
 
+    
 
 
 
